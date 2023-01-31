91e45b5a847eff6725bc021457b14f45272f0887
AzureDuulga <azure.duulga.enkhtur@gmail.com>
2023-01-31 18:51:30 +0800

    gg

---
+++ server.js
@@ -0,0 +1,103 @@
+const express = require("express");
+const cors = require("cors");
+const fs = require("fs");
+const server = express();
+const { v4: uuidv4 } = require("uuid");
+const bcrypt = require("bcrypt");
+
+server.use(cors());
+server.use(express.json());
+
+server.get("/", (req, res) => {
+  res.send("GET huseltend hariu ogloo");
+});
+
+server.get("/users", (req, res) => {
+  fs.readFile("users.json", "utf-8", (err, data) => {
+    if (err) {
+      console.log("File unshihad aldaa zaalaa");
+      return;
+    } else {
+      console.log(data);
+      console.log(JSON);
+      const parsedData = JSON.parse(data);
+      res.status(201).json({ users: parsedData.users });
+    }
+  });
+});
+
+server.get("/users/:id", (req, res) => {
+  const { id } = req.params;
+  console.log(id);
+  const data = fs.readFileSync("users.json", "utf-8");
+  const parsedData = JSON.parse(data);
+  const user = parsedData.users.find((el) => el.id === id);
+  res.status(200).json({ user });
+});
+
+server.put("/users/:id", (req, res) => {
+  const { id } = req.params;
+  const { name, email } = req.body;
+  const data = fs.readFileSync("users.json", "utf-8");
+  const parsedData = JSON.parse(data);
+  const findIndex = parsedData.users.findIndex((el) => el.id === id);
+  parsedData.users[findIndex].name = name;
+  parsedData.users[findIndex].email = email;
+  fs.writeFileSync("users.json", JSON.stringify(parsedData));
+  res
+    .status(201)
+    .json({ message: "Шинэ хэрэглэгчийн өгөгдөл амжилттай солигдлоо." });
+});
+
+server.post("/signup", (req, res) => {
+  const { name, role, email, password } = req.body;
+  const data = fs.readFileSync("users.json", "utf-8");
+  const parsedData = JSON.parse(data);
+  const id = uuidv4();
+  const salted = bcrypt.genSaltSync(10);
+  const hashedPassword = bcrypt.hashSync(password, salted);
+  const newUser = {
+    id,
+    name,
+    role,
+    password: hashedPassword,
+    email,
+  };
+  parsedData.users.push(newUser);
+  fs.writeFileSync("users.json", JSON.stringify(parsedData));
+  res.status(201).json({ message: "Шинэ хэрэглэгчийгн амжилттай бүртгэлээ" });
+});
+
+server.post("/signin", (req, res) => {
+  const { id, email, password } = req.body;
+  const data = fs.readFileSync("users.json", "utf-8");
+  const parsedData = JSON.parse(data);
+  const findUser = parsedData.users.find((user) => user.id === id);
+  if (!findUser) {
+    res.status(401).json({ message: "Ийм хэрэглэгч олдсонгүй" });
+  }
+  const isCheck = bcrypt.compareSync(password, findUser.password);
+  if (isCheck) {
+    es.status(200).json({ message: "Амжилттай нэвтэрлээ.", user: findUser });
+  } else {
+    res
+      .status(401)
+      .json({ message: "Имэйл эсвэл нууц үг буруу байна.", user: null });
+  }
+});
+
+server.delete("/users/:id", (req, res) => {
+  const { id } = req.params;
+  const data = fs.readFileSync("users.json", "utf-8");
+  const parsedData = JSON.parse(data);
+  const findIndex = parsedData.users.findIndex((el) => el.id === id);
+  parsedData.users.splice(findIndex, 1);
+  fs.writeFileSync("users.json", JSON.stringify(parsedData));
+  res
+    .status(201)
+    .json({ message: `${id} тай хэрэглэгч амжилттай устгагдлаа.` });
+});
+
+server.listen(8000, () => {
+  console.log("server aslaa");
+});
